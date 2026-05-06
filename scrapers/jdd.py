2fc973d5cda4d0270ac29a6fbd6b9ea1fd80a12e
corto2corto <kalice.ecr@gmail.com>
2026-05-06 19:33:00 +0200

    JDD succés 1

--- scrapers/jdd.py
+++ scrapers/jdd.py
@@ -1,11 +1,20 @@
-import requests
+from playwright.sync_api import sync_playwright
+from playwright_stealth import stealth_sync
 from bs4 import BeautifulSoup
 
-response = requests.get("https://www.lejdd.fr/Societe/narcotrafic-lombre-des-dealers-plane-sur-les-mairies-173226")
-soup = BeautifulSoup(response.text, "html.parser")
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)  # headless=False = navigateur visible
+    page = browser.new_page()
+    stealth_sync(page)  # masque les traces d'automatisation
+    page.goto("https://www.lejdd.fr/Societe/narcotrafic-lombre-des-dealers-plane-sur-les-mairies-173226")
+    page.wait_for_load_state("domcontentloaded")
+    
+    html = page.content()
+    browser.close()
 
+soup = BeautifulSoup(html, "html.parser")
 paragraphes = soup.find_all("p")
 
-print(response.text[:2000])
-
-# Protection par Cloudflare 
+for p in paragraphes:
+    text = str(p)
+    print(p)
