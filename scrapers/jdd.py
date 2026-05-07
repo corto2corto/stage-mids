63bb34557ccc862d022951e717a630582d108874
corto2corto <kalice.ecr@gmail.com>
2026-05-07 15:25:32 +0200

    v0.0.0 - request

--- scrapers/jdd.py
+++ scrapers/jdd.py
@@ -1,20 +1,9 @@
-from playwright.sync_api import sync_playwright
-from playwright_stealth import stealth_sync
+import requests
 from bs4 import BeautifulSoup
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)  
-    page = browser.new_page()
-    stealth_sync(page)  # masque les traces d'automatisation
-    page.goto("https://www.lejdd.fr/Societe/narcotrafic-lombre-des-dealers-plane-sur-les-mairies-173226")
-    page.wait_for_load_state("domcontentloaded")
-    
-    html = page.content()
-    browser.close()
+response = requests.get("https://www.lejdd.fr/Societe/narcotrafic-lombre-des-dealers-plane-sur-les-mairies-173226")
+soup = BeautifulSoup(response.text, "html.parser")
 
-soup = BeautifulSoup(html, "html.parser")
 paragraphes = soup.find_all("p")
 
-for p in paragraphes:
-    text = str(p)
-    print(p)
+print(response.text[:2000])
