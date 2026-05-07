a623f3e00cb43ee6061b1057f34a13a4ddfcfb33
corto2corto <kalice.ecr@gmail.com>
2026-05-07 15:30:34 +0200

    v0.1.1 - playwright + stealth

--- scrapers/jdd.py
+++ scrapers/jdd.py
@@ -1,14 +1,18 @@
 from playwright.sync_api import sync_playwright
+from playwright_stealth import stealth_sync
+from bs4 import BeautifulSoup
 
 with sync_playwright() as p:
-    browser = p.chromium.launch()
+    browser = p.chromium.launch(headless=False)  # headless=False = navigateur visible
     page = browser.new_page()
+    stealth_sync(page)  # masque les traces d'automatisation
     page.goto("https://www.lejdd.fr/Societe/narcotrafic-lombre-des-dealers-plane-sur-les-mairies-173226")
-    page.wait_for_load_state()
+    page.wait_for_load_state("domcontentloaded")
+    
     html = page.content()
     browser.close()
+    
+    # On télécharge le html dans un fichier, qu'on passera à BS4 ensuite
 
-soup = BeautifulSoup(html, "html.parser")
-paragraphes = soup.find_all("p")
-
-print(len(paragraphes))
+with open("173226.html", "w", encoding="utf-8") as f:
+    f.write(html)
