a9731bf4186bc8320175c8862b24c686c528bf91
corto2corto <kalice.ecr@gmail.com>
2026-05-08 14:00:24 +0200

    bump to v0.2.0 for Paris Match

---
+++ scrapers/paris_match.py
@@ -0,0 +1,20 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+PROFILE_PATH = r"C:\Users\E.E\AppData\Roaming\Mozilla\Firefox\Profiles\m5oos7by.default-release"
+URL = "https://www.parismatch.com/actu/societe/exclusif-larmee-explore-sa-future-arme-anti-drone-268773"
+
+options = Options()
+options.add_argument("-profile")
+options.add_argument(PROFILE_PATH)
+
+with webdriver.Firefox(options=options) as driver:
+    driver.get(URL)
+    driver.delete_all_cookies()
+    driver.get(URL)
+    time.sleep(5)
+    html = driver.page_source
+
+with open(r"docs\paris_match.html", "w", encoding="utf-8") as f:
+    f.write(html)
