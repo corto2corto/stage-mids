0eac7fb448ed27aaba8beb081868cc27d3be8c65
corto2corto <kalice.ecr@gmail.com>
2026-05-25 11:13:31 +0200

    chore: fix cache problem in upload_csv.py

--- data/upload_csv.py
+++ data/upload_csv.py
@@ -1,7 +1,13 @@
+import os
+
+# Indique à HF de stocker son cache ici, car sinon conflit avec le cache de ubuntu/.cache
+os.environ["HF_HOME"] = "/data/elias/hf_cache"
+
 from huggingface_hub import snapshot_download
 
 snapshot_download(
     repo_id="corto2corto/articles",
     repo_type="dataset",
-    local_dir="/data/elias/stage-mids/data"
+    local_dir="/data/elias/stage-mids/data",
+    token="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"
 )
