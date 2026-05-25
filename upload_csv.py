43a5fa137a8cf94d060c8e8bd26400efeb1b0ec2
corto2corto <kalice.ecr@gmail.com>
2026-05-25 09:59:32 +0200

    new: upload_csv.py, MAJ requirement.txt

--- upload_csv.py
+++ upload_csv.py
@@ -3,6 +3,5 @@ from huggingface_hub import snapshot_download
 snapshot_download(
     repo_id="corto2corto/articles",
     repo_type="dataset",
-    local_dir="/data/elias/stage-mids/data",
-    token=""
+    local_dir="/data/elias/stage-mids/data"
 )
